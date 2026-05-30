8cfeab675955c58237442e125a9bec27dc1aebcc
Sumant9761 <142734516+Sumant9761@users.noreply.github.com>
2026-05-30 10:25:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function makeid(l) {
   // write your code here
+	let ans = "";
+	let char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+	for(let i=0; i<l; i++){
+		ans += char_list.charAt{
+			Math.floor(Math.random()*char_list.length)
+		}
+	}
+	return ans;
 }
 
 // Do not change the code below.
